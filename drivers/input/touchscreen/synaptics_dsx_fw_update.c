5eb4c03127d460738e10d8a642eade358a9e9da7
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2024-12-02 19:18:00 +0100

    Revert "synaptics: s3320: update to arter97 version"
    This reverts commit 744efba613d892c7becf68edf28dfe79ac29bb95.

--- drivers/input/touchscreen/synaptics_dsx_fw_update.c
+++ drivers/input/touchscreen/synaptics_dsx_fw_update.c
@@ -39,7 +39,7 @@
 #include <linux/input.h>
 #include <linux/firmware.h>
 #include <linux/platform_device.h>
-#include <linux/input/synaptics_dsx.h>
+#include <linux/input/synaptics_dsx_s3320.h>
 #include "synaptics_dsx_core.h"
 
 #define FW_IHEX_NAME  "tp/fw_synaptics_17819.img"
@@ -58,11 +58,15 @@
 #endif
 
 /*********************for Debug LOG switch*******************/
-// #define DEBUG
 #define TPD_DEVICE "synaptics,s3320_firmware"
 #define TPD_ERR(a, arg...)  pr_err(TPD_DEVICE ": " a, ##arg)
 #define TPDTM_DMESG(a, arg...)  printk(TPD_DEVICE ": " a, ##arg)
-#define TPD_DEBUG(a,arg...)  pr_debug(TPD_DEVICE ": " a,##arg)
+
+#define TPD_DEBUG(a, arg...)\
+	do {\
+		if (tp_debug) \
+		pr_err(TPD_DEVICE ": " a, ##arg);\
+	} while (0)
 
 #define MAX_WRITE_SIZE 4096
 
@@ -1969,7 +1973,6 @@ static int fwu_write_f34_v7_blocks(unsigned char *block_ptr,
 	int retval;
 	unsigned char data_base;
 	unsigned char length[2];
-	unsigned char *buf = NULL;
 	unsigned short transfer;
 	unsigned short remaining = block_cnt;
 	unsigned short block_number = 0;
@@ -1993,8 +1996,6 @@ static int fwu_write_f34_v7_blocks(unsigned char *block_ptr,
 				__func__);
 		return retval;
 	}
-
-	retval = 0;
 
 	do {
 		if (remaining / fwu->payload_length)
@@ -2035,42 +2036,38 @@ static int fwu_write_f34_v7_blocks(unsigned char *block_ptr,
 #endif
 		left_bytes = transfer * fwu->block_size;
 
-		retval = 0;
-		buf = kmalloc(PAGE_SIZE * 2, GFP_KERNEL);
-
 		do {
 			if (left_bytes / max_write_size)
 				write_size = max_write_size;
 			else
 				write_size = left_bytes;
 
-			retval = synaptics_rmi4_reg_write_big(fwu->client,
+			retval = synaptics_rmi4_reg_write(fwu->client,
 					data_base + fwu->off.payload,
-					buf, block_ptr, write_size);
+					block_ptr,
+					write_size);
 			if (retval < 0) {
 			TPD_ERR("%s: Failed to write (remaining = %d)\n",
 						__func__, remaining);
-				goto free_buf;
+				return retval;
 			}
 
 			block_ptr += write_size;
 			left_bytes -= write_size;
 		} while (left_bytes);
+
 		retval = fwu_wait_for_idle(WRITE_WAIT_MS, true);
+
 		if (retval < 0) {
 			TPD_ERR("%s: Failed to wait idle (remaining %d)\n",
 					__func__, remaining);
-			goto free_buf;
+			return retval;
 		}
 
 		remaining -= transfer;
 	} while (remaining);
 
-free_buf:
-	if (buf)
-		kfree(buf);
-
-	return retval;
+	return 0;
 }
 
 static int fwu_write_f34_v5v6_blocks(unsigned char *block_ptr,
@@ -2079,7 +2076,6 @@ static int fwu_write_f34_v5v6_blocks(unsigned char *block_ptr,
 	int retval;
 	unsigned char data_base;
 	unsigned char block_number[] = {0, 0};
-	unsigned char *buf;
 	unsigned short blk;
 
 	data_base = fwu->f34_fd.data_base_addr;
@@ -2096,39 +2092,35 @@ static int fwu_write_f34_v5v6_blocks(unsigned char *block_ptr,
 		return retval;
 	}
 
-	retval = 0;
-	buf = kmalloc(PAGE_SIZE * 2, GFP_KERNEL);
-
 	for (blk = 0; blk < block_cnt; blk++) {
-		retval = synaptics_rmi4_reg_write_big(fwu->client,
+		retval = synaptics_rmi4_reg_write(fwu->client,
 				data_base + fwu->off.payload,
-				buf, block_ptr, fwu->block_size);
+				block_ptr,
+				fwu->block_size);
 		if (retval < 0) {
 			TPD_ERR("%s: Failed to write block data (block %d)\n",
 					__func__, blk);
-			goto free_buf;
+			return retval;
 		}
 
 		retval = fwu_write_f34_command(command);
 		if (retval < 0) {
 			TPD_ERR("%s: Failed to write command for block %d\n",
 					__func__, blk);
-			goto free_buf;
+			return retval;
 		}
 
 		retval = fwu_wait_for_idle(WRITE_WAIT_MS, false);
 		if (retval < 0) {
 			TPD_ERR("%s Failed wait idle status (block %d)\n",
 					__func__, blk);
-			goto free_buf;
+			return retval;
 		}
 
 		block_ptr += fwu->block_size;
 	}
 
-free_buf:
-	kfree(buf);
-	return retval;
+	return 0;
 }
 
 static int fwu_write_f34_blocks(unsigned char *block_ptr,
